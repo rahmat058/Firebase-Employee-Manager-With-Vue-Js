1660c2f3a28cf3b860f71d3454d7d645ea0d2f7d
rahmat058 <md.kazi.rahmat@gmail.com>
2018-10-16 00:21:56 +0600

    Project Initialize and Firebase setup

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDXb0pjKwdQc0E3Z5rBYrT6TSJJOlKD_vE",
+    authDomain: "vuefs-prod-5e995.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-5e995.firebaseio.com",
+    projectId: "vuefs-prod-5e995",
+    storageBucket: "vuefs-prod-5e995.appspot.com",
+    messagingSenderId: "66191059693"
+}
